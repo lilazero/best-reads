b2723d1e27b8ef901414e1e86eca05b67459362b
lilazero <andililajal@gmail.com>
2025-11-28 13:29:30 +0100

    added a wrapper to handle different screen sizes

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
+import Wrapper from "../components/ui/Wrapper";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -29,9 +30,11 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <Header />
-        {children}
-        <Footer />
+        <Wrapper>
+          <Header />
+          {children}
+          <Footer />
+        </Wrapper>
       </body>
     </html>
   );
---
+++ components/ui/Wrapper.tsx
@@ -0,0 +1,7 @@
+export default function Wrapper({ children }: { children: React.ReactNode }) {
+  return (
+    <div className="w-full px-4 sm:px-6 lg:px-8">
+      <div className="mx-auto max-w-7xl lg:w-4/5">{children}</div>
+    </div>
+  );
+}
