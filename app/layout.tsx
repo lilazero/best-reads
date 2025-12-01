63f189039f297fbe198d5cf2e4080b4d31040246
Yarema-Kuz <yarema44@ukr.net>
2025-12-01 10:44:58 +0100

    Moved Footer from component folder deeper into DashboardComponents folder and updated import in layout.tsx to reflect changes

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/Header";
-import Footer from "@/components/Footer";
+import Footer from "@/components/DashboardComponents/Footer";
 import Wrapper from "../components/ui/Wrapper";
 
 const geistSans = Geist({
