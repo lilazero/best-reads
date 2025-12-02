cc67b799eb991b0a87006b94275e53ac46021beb
lilazero <andililajal@gmail.com>
2025-12-02 10:08:24 +0100

    create and add RecommendedBooks component to Home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 "use client";
 
-import SearchBar from "@/components/SearchBar";
+import RecommendedBooks from "@/components/DashboardComponents/RecommendedBooks";
+import SearchBar from "@/components/DashboardComponents/SearchBar";
 
 export default function Home() {
   return (
     <div className="flex min-h-vh mt-10 justify-center font-sans dark:bg-black">
       <SearchBar />
+      <RecommendedBooks />
     </div>
   );
 }
---
+++ components/DashboardComponents/RecommendedBooks.tsx
@@ -0,0 +1,7 @@
+export default function RecommendedBooks() {
+  return (
+    <>
+      <div>Recommended Books Component</div>
+    </>
+  );
+}
