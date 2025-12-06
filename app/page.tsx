7e081911231001ec22e1e5f2c633859da0ae40b3
Andi Lilaj <andililajal@gmail.com>
2025-12-06 15:16:00 +0100

    style: adjust layout and spacing in Home and SearchBar components

--- app/page.tsx
+++ app/page.tsx
@@ -6,10 +6,10 @@ import { RecommendedBooks } from "@/lib/mockData";
 
 export default function Home() {
   return (
-    <div className="flex items-center flex-col min-h-vh mt-10 justify-center font-sans dark:bg-black">
+    <div className="flex items-center flex-col min-h-vh mt-2 justify-center font-sans dark:bg-black">
       <SearchBar />
-      <h2 className="text-2xl font-bold mb-6 mt-20 px-4">Recommended Books</h2>
-      {/* Instead of the /mockData/RecommendedBooks add the ones from MongoDB */}
+      <h2 className="text-2xl font-bold  mt-6 px-4">Recommended Books</h2>
+      {/* TODO: Instead of the /mockData/RecommendedBooks add the ones from MongoDB */}
       <BookCardList books={RecommendedBooks} showBuyButton={false} />
     </div>
   );
--- components/DashboardComponents/SearchBar.tsx
+++ components/DashboardComponents/SearchBar.tsx
@@ -10,28 +10,26 @@ import {
 import { tags } from "../../lib/mockData";
 export default function SearchBar() {
   return (
-    <>
-      <div className="w-full max-w-2xl">
-        <Autocomplete items={tags} autoHighlight>
-          <AutocompleteInput
-            id="tags"
-            placeholder="exempli gratia: Code Complete"
-            className="mt-2 bg-white"
-          />
-          <AutocompletePositioner sideOffset={6}>
-            <AutocompletePopup>
-              <AutocompleteEmpty>No tags found.</AutocompleteEmpty>
-              <AutocompleteList>
-                {(tag) => (
-                  <AutocompleteItem key={tag.id} value={tag.value}>
-                    {tag.value}
-                  </AutocompleteItem>
-                )}
-              </AutocompleteList>
-            </AutocompletePopup>
-          </AutocompletePositioner>
-        </Autocomplete>
-      </div>
-    </>
+    <div className="w-full max-w-2xl">
+      <Autocomplete items={tags} autoHighlight>
+        <AutocompleteInput
+          id="tags"
+          placeholder="exempli gratia: Code Complete"
+          className="mt-2 bg-white"
+        />
+        <AutocompletePositioner sideOffset={6}>
+          <AutocompletePopup>
+            <AutocompleteEmpty>No tags found.</AutocompleteEmpty>
+            <AutocompleteList>
+              {(tag) => (
+                <AutocompleteItem key={tag.id} value={tag.value}>
+                  {tag.value}
+                </AutocompleteItem>
+              )}
+            </AutocompleteList>
+          </AutocompletePopup>
+        </AutocompletePositioner>
+      </Autocomplete>
+    </div>
   );
 }
