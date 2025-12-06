8a03254fcfbf0080344b220e5eb8e924458212d9
Andi Lilaj <andililajal@gmail.com>
2025-12-06 18:16:54 +0100

    How did Yarema Manage to find a http link on amazon?

--- next.config.ts
+++ next.config.ts
@@ -11,6 +11,11 @@ const nextConfig: NextConfig = {
         protocol: "https",
         hostname: "m.media-amazon.com",
       },
+      //how did yarema manage to find a http image on amazon, is beyond me. but i figured it out fast
+      {
+        protocol: "http",
+        hostname: "m.media-amazon.com",
+      },
       {
         protocol: "https",
         hostname: "www.goodreads.com",
