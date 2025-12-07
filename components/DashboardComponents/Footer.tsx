f5c14b6d4bf1e41b7023d23ad4255d2b32f31c6e
Andi Lilaj <andililajal@gmail.com>
2025-12-07 14:51:03 +0100

    fix: also footer background, should also change colors. meh

--- components/DashboardComponents/Footer.tsx
+++ components/DashboardComponents/Footer.tsx
@@ -1,6 +1,6 @@
 export default function Footer() {
   return (
-    <footer className="bg-gray-100 text-gray-800 ">
+    <footer className="text-gray-800 bg-primary-background">
       <div className="max-w-7xl mx-auto px-6 py-10">
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-8">
           <div>
